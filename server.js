5af72fd3f5b5d68e1197e02025bc06be01ebcfb0
CodeWithRamkumar <ramkumar03251999@gmail.com>
2026-01-04 14:00:47 +0530

    Initial commit

--- server.js
+++ server.js
@@ -10,7 +10,10 @@ const app = express();
 const PORT = process.env.PORT || 3001;
 
 // Middleware
-app.use(cors());
+app.use(cors({
+  origin: ['http://localhost:8100', 'http://localhost:3000', 'https://codewithramkumar.github.io'],
+  credentials: true
+}));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
